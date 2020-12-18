1a226a5e7396970f1fad24a46d545824fdce1d8a
boris-kolesnikov <boris.kolesnikov.1997@gmail.com>
2020-12-18 15:23:54 +0300

    Update metrology_chain.py

--- metrology_chain.py
+++ metrology_chain.py
@@ -18,8 +18,10 @@ while exit == False:
         if toleranc_percente <= 0:
                 toleranc_percente_eqals=(100*tolerance/nominal_value)
                 print(('Это будет ±'), toleranc_percente_eqals, ('%'))
+                
         print('Укажите наименование средства измерений [    ]')
         device_name = (input())
+        
         print('и его погрешность')
         deflection=float(input())
         print('Погрешность в процентах? Введите 1 если "ДА",  0 если  "НЕТ"')
@@ -28,21 +30,21 @@ while exit == False:
         if deflection_percente <= 0:
                 deflection_percente_eqals=(100*deflection/nominal_value)
                 print(('Это будет ±'), deflection_percente_eqals, ('%'))
+                
         print('Укажите требуемый коэффициен точности')
         accuracy_ratio=float(input())     
         result=tolerance/deflection
         if result < accuracy_ratio: print('ВНИМАНИЕ! КТ меньше требуемого! ')
         if result >= accuracy_ratio: print('Поздравляю! КТ соответствует требованию!')
+                
         print(' ')
         print('Ваша метрологическая цепь имеет вид: ')
         print(" ")
         print("  ", parameter_name)
         print(('     НОМИНАЛЬНОЕ ЗНАЧ.:'), nominal_value)
-
         if toleranc_percente >= 1: print('     ДОПУСК: ±', tolerance, ' %')
         if toleranc_percente <= 0: print('     ДОПУСК: ±', toleranc_percente_eqals, ' %')
         print(('     НАЗВАНИЕ СИ:'), device_name)
-
         if deflection_percente >= 1: print('     ПОГРЕШНОСТЬ СИ: ±', deflection, ' %')
         if deflection_percente <= 0: print('     ПОГРЕШНОСТЬ СИ: ±', deflection_percente_eqals, ' %')
 
